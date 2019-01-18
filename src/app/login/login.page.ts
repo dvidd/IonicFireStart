588b1917ec7e9f63ca1e83d2edec221d805c4507
David L Barrero <>
2019-01-18 14:17:25 +0100

    Update login.page.ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+
+// https://medium.com/@ryangordon210/integrating-google-login-to-your-ionic-app-with-firebase-771cf3d50957
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
